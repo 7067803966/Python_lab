ee6b58eb1687068be932964a8b47fc003313d667
7067803966 <87516728+7067803966@users.noreply.github.com>
2022-02-02 10:40:10 +0530

    Add files via upload

---
+++ read_file.py
@@ -0,0 +1,15 @@
+f = open('sample.txt', 'r')
+# f = open('try.txt', 'r')
+
+data = f.readlines()
+count = 0
+count_ele = 0
+for lines in data:
+    # print(lines)
+    count += 1
+    for element in lines:
+        count_ele += 1
+
+print('Total number of element is:', count_ele)
+print("Total number of lines is:", count)
+f.close()
